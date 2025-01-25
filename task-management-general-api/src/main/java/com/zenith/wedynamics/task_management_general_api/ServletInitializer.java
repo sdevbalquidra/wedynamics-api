4c9d50651feecc321b474a23ae2b26d8371569e1
sdevbalquidra <sherwin.balquidra+dev@gmail.com>
2025-01-25 14:46:17 +0800

    Add ServletInitializer

---
+++ task-management-general-api/src/main/java/com/zenith/wedynamics/task_management_general_api/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.zenith.wedynamics.task_management_general_api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TaskManagementGeneralAPI.class);
+	}
+
+}
